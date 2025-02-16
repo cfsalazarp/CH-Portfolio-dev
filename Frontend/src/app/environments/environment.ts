8b65cfd2a66c85ecf89184576a8a146f27133657
cfsalazarp <christiansalazar1129@gmail.com>
2025-02-16 18:17:34 -0500

    feat: se actualiza variable de ambiente

--- Frontend/src/app/environments/environment.ts
+++ Frontend/src/app/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    STRAPI_HOST: "https://dev-portfolio-be-strapi.onrender.com",
+    STRAPI_HOST: 'https://ch-portfolio-dev-be.onrender.com',
     STRAPI_TOKEN: process.env['STRAPI_TOKEN'] ?? '',
 };
