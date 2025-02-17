ccf2dc548dd336bb703088eabf6ecc02740d32eb
cfsalazarp <christiansalazar1129@gmail.com>
2025-02-17 11:31:20 -0500

    feat (footer): Creación de footer con estilos

--- Frontend/src/app/components/footer/footer.component.ts
+++ Frontend/src/app/components/footer/footer.component.ts
@@ -1,11 +1,34 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Social } from '@app/core/models/social.model';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
 
+  socialLinks: Social[] = [
+      {
+        id: 1,
+        name: 'GitHub',
+        slug: 'github',
+        url: 'https://github.com/cfsalazarp',
+      },
+      {
+        id: 2,
+        name: 'LinkedIn',
+        slug: 'linkedin',
+        url: 'https://www.linkedin.com/in/cfsalazarp/',
+      },
+      {
+        id: 3,
+        name: 'Correo',
+        slug: 'mail',
+        url: 'mailto:christiansalazar1129@gmail.com',
+      }
+    ];
+    
 }
