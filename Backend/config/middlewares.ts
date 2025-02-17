5301cf38e53c2a23e3949ee0d700127999256a27
cfsalazarp <christiansalazar1129@gmail.com>
2025-02-16 19:09:33 -0500

    Update middlewares.ts

--- Backend/config/middlewares.ts
+++ Backend/config/middlewares.ts
@@ -1,38 +1,13 @@
 export default [
   "strapi::logger",
   "strapi::errors",
-  {
-    name: "strapi::security",
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          "connect-src": ["'self'", "https:"],
-          "img-src": [
-            "'self'",
-            "data:",
-            "blob:",
-            "market-assets.strapi.io",
-            "res.cloudinary.com",
-          ],
-          "media-src": [
-            "'self'",
-            "data:",
-            "blob:",
-            "market-assets.strapi.io",
-            "res.cloudinary.com",
-          ],
-          upgradeInsecureRequests: null,
-        },
-      },
-    },
-  },
+  "strapi::security",
   {
     name: 'strapi::cors',
     config: {
       enabled: true,
       headers: '*',
-      origin: ['https://ch-portfolio-dev.netlify.app', 'http://localhost:1337'], // Permitir Netlify y localhost
+      origin: ['*'], // Permitir Netlify y localhost
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
       credentials: true,
     },
