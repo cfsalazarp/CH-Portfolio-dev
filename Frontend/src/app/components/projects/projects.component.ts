c69d049b4d7b0093d98aae891927ddd6060e73a6
csalaz5 <csalaz5@bancodebogota.com.co>
2025-02-27 20:32:02 -0500

    Update projects.component.ts

--- Frontend/src/app/components/projects/projects.component.ts
+++ Frontend/src/app/components/projects/projects.component.ts
@@ -210,20 +210,20 @@ export class ProjectsComponent {
 
   ngOnInit() {
     console.log("ProjectsComponent: ngOnInit");
-    // this.projectsService.getProjects().subscribe((data) => {
-    //   console.log("data: ", data)
-    //   this.projects = data.map((project: any) => {
-    //     return {
-    //       id: project.id,
-    //       title: project.title,
-    //       description: project.description,
-    //       repo_url: project.repo_url,
-    //       live_url: project.live_url,
-    //       image: project.image != null ? `${environment.STRAPI_HOST}${project.image.url}` : null,
-    //       technologies: project.technologies.map((technology: any) => technology.name)
-    //     }
-    //   }).sort((a: Project, b: Project) => a.id - b.id);
-    // });
+    this.projectsService.getProjects().subscribe((data) => {
+      console.log("data: ", data)
+      this.projects = data.map((project: any) => {
+        return {
+          id: project.id,
+          title: project.title,
+          description: project.description,
+          repo_url: project.repo_url,
+          live_url: project.live_url,
+          image: project.image != null ? `${environment.STRAPI_HOST}${project.image.url}` : null,
+          technologies: project.technologies.map((technology: any) => technology.name)
+        }
+      }).sort((a: Project, b: Project) => a.id - b.id);
+    });
   }
 
   
